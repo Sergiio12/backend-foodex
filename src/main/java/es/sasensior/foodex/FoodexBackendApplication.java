418727e363f5ab0676d8abb7ec7b719e8596e064
Sergio A. Rodriguez <sergio120604@gmail.com>
2025-02-06 10:27:19 +0100

    Proyecto inicializado

---
+++ src/main/java/es/sasensior/foodex/FoodexBackendApplication.java
@@ -0,0 +1,13 @@
+package es.sasensior.foodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodexBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/sasensior/foodex/FoodexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.sasensior.foodex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
